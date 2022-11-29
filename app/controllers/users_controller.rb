2796410242e1b9d34184d5b199b92f1c90f570fa
lilian <lilian.wangechi@student.moringaschool.com>
2022-11-29 10:08:10 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def show
+    users= User.find_by(id: params[:id])
+    render json: users, include: :items
+  end
 end
+
+# def show
+#   users = User.find_by(id:params[:id])
+
+#   if users
+#       render json: users, status: :ok
+#   else
+#       render json: {error: "user not found"}, status: :not_found
+# end
+# end
+# end
